8e21e8275a8949bc154df4d0504fc2bcdca90d3c
Elizabete Silva de Paula <elizabete.silvapaula@gmail.com>
2021-09-28 00:54:08 +0000

    Implemented the function to generate Randon String

--- express_server.js
+++ express_server.js
@@ -1,16 +1,29 @@
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+function generateRadomString(length, chars) {
+  let result = "";
+  for (let i = length; i > 0; --i)
+    result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+}
+console.log(
+  generateRadomString(
+    5,
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  )
+);
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
@@ -23,6 +36,11 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/new", (req, res) => {
