70fe5db1b05f3908f59a85031d9b9568c7f8c243
Elizabete Silva de Paula <elizabete.silvapaula@gmail.com>
2021-09-28 00:21:38 +0000

    Added a GET Route to Show the Form - url_new

--- express_server.js
+++ express_server.js
@@ -1,6 +1,10 @@
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -19,6 +23,10 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
