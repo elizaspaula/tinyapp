5f940bca3c8002dc5811c1a0e32714b4c04000bc
Elizabete Silva de Paula <elizabete.silvapaula@gmail.com>
2021-09-30 01:19:56 +0000

    Created the registration get and post and refactored the login page

--- express_server.js
+++ express_server.js
@@ -8,6 +8,31 @@ app.use(cookieParser());
 app.use(morgan("dev"));
 app.set("view engine", "ejs");
 
+const urlDatabase = {
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
+};
+
+const usersDatabase = {
+  1234: {
+    id: "1234",
+    email: "elizabete",
+    password: "abc",
+  },
+};
+
+// Check if the username it's on the database
+const findUserByEmail = (email) => {
+  for (const userId in usersDatabase) {
+    const user = usersDatabase[userId];
+    if (user.email === email) {
+      return user;
+    }
+  }
+  return null;
+};
+
+// Function to generate Randon ID
 function generateRadomString(
   length,
   chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -18,9 +43,14 @@ function generateRadomString(
   return result;
 }
 
-const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
+// Check if current cookies correspond with the user in the database
+const cookieHasUser = function (cookies, usersDatabase) {
+  for (const user in usersDatabase) {
+    if (cookies === true) {
+      return true;
+    }
+    return false;
+  }
 };
 
 const bodyParser = require("body-parser");
@@ -35,12 +65,43 @@ app.get("/register", (req, res) => {
   res.render("urls_register");
 });
 
+// // POST Register page
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // check if the email and password are not blank
+  if (!email || !password) {
+    return res.status(400).send("email or password cannot be blank");
+  }
+
+  // check to see if email exists in database already
+  const user = findUserByEmail(email);
+  if (user) {
+    return res.status(400).send("user with that email currently exists");
+  }
+
+  const id = generateRadomString(4);
+
+  usersDatabase[id] = {
+    id: id,
+    email: email,
+    password: password,
+  };
+
+  console.log("user", usersDatabase);
+  res.cookie("UserId", id);
+  res.redirect("/urls");
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  const userId = req.cookies["UserId"];
+  const user = usersDatabase[userId];
+  const templateVars = { urls: urlDatabase, user: user };
   res.render("urls_index", templateVars);
 });
 
@@ -51,15 +112,19 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const userId = req.cookies["UserId"];
+  const user = usersDatabase[userId];
+  const templateVars = { user };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
+  const userId = req.cookies["UserId"];
+  const user = usersDatabase[userId];
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
-    username: req.cookies["username"],
+    user,
   };
   res.render("urls_show", templateVars);
 });
@@ -88,18 +153,43 @@ app.post("/urls/:shortURL/edit", (req, res) => {
   res.redirect("/urls/" + shortUrlID);
 });
 
-//Login route
+// GET Login rout
+app.get("/login", (req, res) => {
+  res.render("urls_login");
+});
+
+//POST Login route
 
 app.post("/login", (req, res) => {
-  const username = req.body.username;
-  res.cookie("username", username);
+  console.log(req.body);
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // check if client sent down blank email or password
+  if (!email || !password) {
+    return res.status(403).send("email or password cannot be blank");
+  }
+
+  const user = findUserByEmail(email);
+
+  // if that user exists with that email
+  if (!user) {
+    return res.status(403).send("no user with that email was found");
+  }
+
+  // does the password provided from the request
+  // match the password of the user
+  if (user.password !== password) {
+    return res.status(403).send("password does not match");
+  }
+
+  res.cookie("UserId", user.id);
   res.redirect("/urls/");
 });
 
 //Logout route
 app.post("/logout", (req, res) => {
-  const username = req.body.username;
-  res.clearCookie("username", username);
+  res.clearCookie("UserId");
   res.redirect("/urls/");
 });
 app.listen(PORT, () => {
